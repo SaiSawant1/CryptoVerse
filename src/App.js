c43f8ac8d5596b568af7b3fd1e5fc4968caaee98
Sai Sawant <saisawant2003@gmail.com>
2023-04-04 12:33:07 +0530

    setting up of routes links and styles completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import React from 'react'
-import { Switch,Route,Link } from "react-router-dom" 
-import { Layout,Typography,Space } from 'antd'
-import { Navbar } from './components'
-import "./App.css"
+import React from "react";
+import { Switch, Route, Link, Router, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  News,
+  Cryptocurrencies,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
 const App = () => {
   return (
-    <div className='app'>
-        <div className='navbar'>
-            <Navbar/>
-        </div>
-        <div className='main'>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/news" element={<News />} />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+            </Routes>
+          </div>
+        </Layout>
 
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse
+            <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Homepage</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            <Link to="/news">News</Link>
+          </Space>
         </div>
-        <div className='footer'>
-
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM  from "react-dom";
 import App from "./App"
 import { BrowserRouter as Router } from "react-router-dom";
-
+import 'antd/dist/reset.css';
 ReactDOM.render(<Router><App/></Router> ,document.getElementById("root"))
